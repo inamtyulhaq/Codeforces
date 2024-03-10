c7ccf9778727efcb10c33ccd1bdc658481119035
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-10 17:54:11 +0500

    Create 1742A - Sum.cpp

---
+++ Difficulty. 800/1742A - Sum.cpp
@@ -0,0 +1,30 @@
+// Question
+// 
+
+// Explanation:
+// input three integers
+// use if else to see which one is the sum of other two.
+// there will be three conditions using OR operator.
+// if anyone's true, simply output YES else NO
+
+// Solution:
+#include <iostream>
+using namespace std;
+int main() {
+  int t, a, b, c;
+  cin >> t;
+  while(t--)
+  {
+    cin >> a >> b >> c;
+    if(a+b==c || a+c==b || b+c==a)
+    {
+      cout << "YES" << endl;
+    }
+    else
+    {
+      cout << "NO" << endl;
+    }
+  }
+
+  return 0;
+}
