626cf6bc71da524723f3cb7c4efe866080af5f39
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-12 14:09:03 +0500

    Create 100C - A+B.cpp

---
+++ Difficulty. 1400/100C - A+B.cpp
@@ -0,0 +1,17 @@
+// Question
+// https://codeforces.com/problemsets/acmsguru/problem/99999/100
+
+// Explanation:
+// Simply cout the sum.
+
+// Solution:
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
