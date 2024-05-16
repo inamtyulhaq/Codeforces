ed5be6317a7e3f58c7895f3c9e223942135ca6e4
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-16 06:16:51 +0500

    Create I - Welcome for you with Conditions.cpp

---
+++ Difficulty. 800/I - Welcome for you with Conditions.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+// simple, if else
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    if (A >= B)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    return 0;
+}
