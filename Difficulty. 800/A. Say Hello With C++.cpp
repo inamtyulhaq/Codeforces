4ee9adaa2d674ab5af26a41209bb6bd3f98ae987
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-18 11:38:35 +0500

    Create A. Say Hello With C++.cpp

---
+++ Difficulty. 800/A. Say Hello With C++.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+// Hello
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    cout << "Hello, " << str << endl;
+    return 0;
+}
