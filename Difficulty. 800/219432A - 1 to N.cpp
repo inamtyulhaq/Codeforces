9b50ac0a37bcaffed2048610d5959382dc1348cd
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-28 11:47:57 +0500

    Create 219432A - 1 to N.cpp

---
+++ Difficulty. 800/219432A - 1 to N.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A
+
+// one simple for loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
