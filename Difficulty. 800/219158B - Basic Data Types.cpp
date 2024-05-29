dca3fb1a787b121a0325a7c2c6dd33535790e3d8
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-29 21:46:22 +0500

    Create 219158B - Basic Data Types.cpp

---
+++ Difficulty. 800/219158B - Basic Data Types.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+// simply cin and cout the values
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    long long M;
+    char C;
+    float X;
+    double Z;
+    cin >> N >> M >> C >> X >> Z;
+    cout << N << M << C << X << Z;
+    return 0;
+}
