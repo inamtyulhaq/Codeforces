910804fc96ed6353a40830bb21f80bf2ce8087b1
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-30 19:26:01 +0500

    Create 219158C - Simple Calculator.cpp

---
+++ Difficulty. 800/219158C - Simple Calculator.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+
+// do focus on formatting, else problem is easy and clear
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long X, Y;
+    cin >> X >> Y;
+    cout << X << " + " << Y << " = " << X + Y << endl;
+    cout << X << " * " << Y << " = " << X * Y << endl;
+    cout << X << " - " << Y << " = " << X - Y << endl;
+    return 0;
+}
