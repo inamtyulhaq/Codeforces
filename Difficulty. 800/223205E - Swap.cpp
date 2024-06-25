deeb97a91bdcebbb3bd302e0baf0efb221217f66
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-25 14:27:23 +0500

    Create 223205E - Swap.cpp

---
+++ Difficulty. 800/223205E - Swap.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
+
+// simply print Y first and then X
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+    cout << Y << " " << X << endl;
+    return 0;
+}
