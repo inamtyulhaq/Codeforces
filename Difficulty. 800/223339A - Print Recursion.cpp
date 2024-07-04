18b08959ea24a18741d02670b7e4131a17b38da4
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-07-04 16:50:53 +0500

    Create 223339A - Print Recursion.cpp

---
+++ Difficulty. 800/223339A - Print Recursion.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/A
+
+// simply use a while loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    while (N--)
+    {
+        cout << "I love Recursion\n";
+    }
+    return 0;
+}
