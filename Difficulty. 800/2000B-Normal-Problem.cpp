56b0109ed1ab14ba0d3b96426db23f8079720466
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2024-12-15 22:42:56 +0500

    easy problem 2044-A

---
+++ Difficulty. 800/2000B-Normal-Problem.cpp
@@ -0,0 +1,31 @@
+//
+
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string a;
+        cin >> a;
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a[i] == 'p')
+            {
+                a[i] = 'q';
+            }
+            else if (a[i] == 'q')
+            {
+                a[i] = 'p';
+            }
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
---
+++ Difficulty. 800/2044A-Easy-Problem.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/contest/2044/problem/A
+
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << n - 1 << endl;
+    }
+    return 0;
+}
